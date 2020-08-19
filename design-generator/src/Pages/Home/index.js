3a3277e10cf2868edf0c59d8122dbb016215b703
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-08-19 16:55:24 -0300

    Page - Home

--- design-generator/src/Pages/Home/index.js
+++ design-generator/src/Pages/Home/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
+import "./styles.css";
+
 export default function Home() {
     return (
-        <div className='container'>
-            <h1>Home</h1>
+        <div className='container-home'>
+            <h1>&lt;Design_generator&gt;</h1>
+            <Link to='/Dashboard'>Generate</Link>
+            <p className='creator'>
+                Created by Vinícius de Ataídes Bittencourt
+            </p>
         </div>
     );
 }
